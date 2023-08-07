85d294fc56dcaaeae5525b755e594da32a7f2b3d
Denis <moiseev-denis@mail.ru>
2023-08-08 01:02:20 +0300

    Решил задачу 4

---
+++ Task04/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа для отображение чисел от -N До N. Число N вводит пользователь.
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+
+while (i<=num) {
+    Console.WriteLine(i);
+    i += 1;
+}
