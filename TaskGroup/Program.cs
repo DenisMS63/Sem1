c9334f4783d735c6bb92c2c1f969a62566a3c096
Denis <moiseev-denis@mail.ru>
2023-08-07 15:31:07 +0300

    Написал прог проверки двух чисел

---
+++ TaskGroup/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа принимает на входе два числа и определяет
+// является ли первое число квадратом второгого числа
+Console.WriteLine("Введите первое число");
+int big = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int small = Convert.ToInt32(Console.ReadLine());
+
+int square = small * small;
+
