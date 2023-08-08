999e082db1c6820316f0e7821285db31034cfc3f
Denis <moiseev-denis@mail.ru>
2023-08-08 21:20:02 +0300

    Решил задачу 6

---
+++ Task06/Program.cs
@@ -0,0 +1,12 @@
+﻿//Программа принимает на вход число
+// и определяет четное оно или нечетное
+
+Console.WriteLine("Введите число");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int x = n1 % 2;
+
+if (x == 0)
+{
+    Console.WriteLine($"Введеное число - четное");
+}
+else { Console.WriteLine($"Введеное число -  нечетное"); }
