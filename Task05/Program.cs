8ed59cfe5c34498219d79160ff58cd1ec63a86a9
Denis <moiseev-denis@mail.ru>
2023-08-08 14:34:51 +0300

    Добавил Task05

---
+++ Task05/Program.cs
@@ -0,0 +1,10 @@
+﻿//Программа для отображение чисел от -N До N. Число N вводит пользователь.
+
+Console.WriteLine("Введите целое положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+
+while (i<=num) {
+    Console.Write($" {i}");
+    i += 1;
+}
---
+++ Task07/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает на вход трехзначное число 
+// и выводит последнюю цифру
+
+Console.WriteLine("Введите любое трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 0) num *= -1;
+
+if (num > 99 && num < 1000)
+{
+    int lastDigit = num % 10;
+    Console.WriteLine($"Последняя цифра введеного числа = {lastDigit}");
+}
+else Console.WriteLine("Введено некорректное число");
