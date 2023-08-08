f75399b9485b98df93c4eabfd480cf6a03fe1c6b
Denis <moiseev-denis@mail.ru>
2023-08-08 21:22:18 +0300

    Решил задачу 8

---
+++ Task08/Program.cs
@@ -0,0 +1,11 @@
+﻿//Программа принимает на вход число N 
+//и выводит все четные числа от 1 до N
+
+Console.WriteLine("Введите положительное число N");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Четные числа от 1 до N");
+for (int i = 2; i <= n1; i += 2)
+{
+    Console.Write($" {i}");
+}
