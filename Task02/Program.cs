8db066fa8749aae9f3f1e7838baee5cfb1fd3843
Denis <moiseev-denis@mail.ru>
2023-08-08 21:13:46 +0300

    Решил задачу 2

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,24 +1,18 @@
-﻿// Программа принимает на входе два числа и определяет
-// является ли первое число квадратом второгого числа
+﻿// Программа получает от пользователя два числа и выводит max и min
 
-try {
 Console.WriteLine("Введите первое число");
-int big = Convert.ToInt32(Console.ReadLine());
+int n1 = Convert.ToInt32(Console.ReadLine());
+
 Console.WriteLine("Введите второе число");
-int small = Convert.ToInt32(Console.ReadLine()); 
+int n2 = Convert.ToInt32(Console.ReadLine());
 
 
-
-int square = small * small;
-
-if (square == big) 
-{Console.WriteLine($"Число {big} является квадратом числа {small}");}
-
-else 
-{Console.WriteLine($"Число {big} не является квадратом числа {small}");}
+if (n1 > n2)
+{
+    Console.WriteLine($"MAX = {n1} MIN = {n2}");
 }
-
-catch {Console.WriteLine("Введено некорректное значение!");}
-
-
-
+else if (n2 > n1)
+{
+    Console.WriteLine($"MAX = {n2} MIN = {n1}");
+}
+else { Console.WriteLine($"ERROR"); }
