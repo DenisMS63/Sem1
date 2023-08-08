927bcd7db271e5a8efe0cab0f2136cf01446637e
Denis <moiseev-denis@mail.ru>
2023-08-08 13:53:15 +0300

    Добавил файл Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа принимает на входе два числа и определяет
+// является ли первое число квадратом второгого числа
+
+try {
+Console.WriteLine("Введите первое число");
+int big = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int small = Convert.ToInt32(Console.ReadLine()); 
+
+
+
+int square = small * small;
+
+if (square == big) 
+{Console.WriteLine($"Число {big} является квадратом числа {small}");}
+
+else 
+{Console.WriteLine($"Число {big} не является квадратом числа {small}");}
+}
+
+catch {Console.WriteLine("Введено некорректное значение!");}
+
+
+
