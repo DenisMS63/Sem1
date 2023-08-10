82e84392c35fd19c02efa13b73a7c7e484e59023
Denis <moiseev-denis@mail.ru>
2023-08-10 12:15:31 +0300

    Корректировка задачи 9 глобальные переменные

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,10 +1,10 @@
 ﻿//Программа выводи случайное число на отрезке от 10 до 99
 // и показывает наибольшую цифру этого числа
 
-int num = new Random().Next(10, 100);
+int number = new Random().Next(10, 100);
 
 Console.WriteLine($"Случайное число на отрезке от 10 до 99");
-Console.WriteLine($"Число - {num}");
+Console.WriteLine($"Число - {number}");
 
 // int num2 = num % 10;
 // int num3 = num / 10;
@@ -18,8 +18,11 @@ Console.WriteLine($"Число - {num}");
 // int result = num2 > num3 ? num2 : num3;
 // Console.WriteLine($"Наибольшая цифра - {result}");
 
-int maxDigit = MaxDigit(num);
+int maxDigit = MaxDigit(number);
 Console.WriteLine($"Наибольшая цифра - {maxDigit}");
+
+int maxDigit1 = MaxDigit(12);
+Console.WriteLine($"Наибольшая цифра - {maxDigit1}");
 
 int MaxDigit(int num)
 {
