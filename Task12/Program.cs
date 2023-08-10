6fe92a5cb7eaf22e9b634feb9cdfc73943a2fcf1
Denis <moiseev-denis@mail.ru>
2023-08-10 13:49:00 +0300

    Решил задачу 12

---
+++ Task12/Program.cs
@@ -0,0 +1,24 @@
+﻿// Программа принимает на вход 2 числа и выводит является ли
+// первое число крастным второму. Если не кратно то выводится 
+// остаток от деления.
+
+Console.WriteLine("Проверим кратность первого числа относительного второго числа");
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+int MultiplicityTwoNumbers(int n1, int n2)
+{
+    int num = n1 % n2;
+
+    return num;    
+}
+
+int result = MultiplicityTwoNumbers(num1, num2);
+
+
+if (result == 0) Console.WriteLine("Кратно");
+else Console.WriteLine($"Некратно, остаток от деления - {result}");
