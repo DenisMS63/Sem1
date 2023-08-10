0d85599e6b10d631aea598a654d876b97440f521
Denis <moiseev-denis@mail.ru>
2023-08-10 12:47:21 +0300

    Решил задачу 11

---
+++ Task11/Program.cs
@@ -0,0 +1,18 @@
+﻿//Программа генерирует случайное трехзначное число 
+// и удаляет из него вторую цифру
+
+int rand = new Random().Next(100, 1000);
+Console.WriteLine($"Случайное трехзначное число - {rand}");
+
+
+int number = GetNumber(rand);
+Console.WriteLine($"Трехзначное число без второй цифры - {number}");
+
+int GetNumber (int num)
+{
+    int num1 = rand / 100;
+    int num2 = rand % 10;
+    int result = num1 * 10 + num2;
+
+    return result;
+}
