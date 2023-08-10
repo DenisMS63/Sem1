4f9c4141ae5fe36e2ff96d4a9fbbabff039626d5
Denis <moiseev-denis@mail.ru>
2023-08-10 12:10:21 +0300

    Корректировка задачи 9 решение черед метод (функцию)

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -1,13 +1,13 @@
 ﻿//Программа выводи случайное число на отрезке от 10 до 99
 // и показывает наибольшую цифру этого числа
 
-int num = new Random().Next(10, 100); 
+int num = new Random().Next(10, 100);
 
 Console.WriteLine($"Случайное число на отрезке от 10 до 99");
 Console.WriteLine($"Число - {num}");
 
-int num2 = num % 10;
-int num3 = num/10;
+// int num2 = num % 10;
+// int num3 = num / 10;
 
 // if (num3 > num2) 
 // {
@@ -15,5 +15,15 @@ int num3 = num/10;
 // }
 // else Console.WriteLine($"Наибольшая цифра числа {num} это {num2}");
 
-int result = num2 > num3 ? num2 : num3;
-Console.WriteLine($"Наибольшая цифра - {result}");
+// int result = num2 > num3 ? num2 : num3;
+// Console.WriteLine($"Наибольшая цифра - {result}");
+
+int maxDigit = MaxDigit(num);
+Console.WriteLine($"Наибольшая цифра - {maxDigit}");
+
+int MaxDigit(int num)
+{
+    int num2 = num % 10;
+    int num3 = num / 10;    
+    return num2 > num3 ? num2 : num3;
+}
