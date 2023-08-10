ac70cf1f4c03b746aa4704f13274ce1950aafcee
Denis <moiseev-denis@mail.ru>
2023-08-10 11:55:47 +0300

    Корректировка задачи 9 через Тернарный оператор

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -9,8 +9,11 @@ Console.WriteLine($"Число - {num}");
 int num2 = num % 10;
 int num3 = num/10;
 
-if (num3 > num2) 
-{
-    Console.WriteLine($"Наибольшая цифра - {num3}");
-}
-else Console.WriteLine($"Наибольшая цифра числа {num} это {num2}");
+// if (num3 > num2) 
+// {
+//     Console.WriteLine($"Наибольшая цифра - {num3}");
+// }
+// else Console.WriteLine($"Наибольшая цифра числа {num} это {num2}");
+
+int result = num2 > num3 ? num2 : num3;
+Console.WriteLine($"Наибольшая цифра - {result}");
