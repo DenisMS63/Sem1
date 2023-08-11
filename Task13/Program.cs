c7910779e0588b14128e59f53ea34616cb295f0c
Denis <moiseev-denis@mail.ru>
2023-08-11 13:01:26 +0300

    Решил задачу 13

---
+++ Task13/Program.cs
@@ -0,0 +1,27 @@
+﻿// Программа принимает на вход число и сообщает третью цифру этого числа
+// либо сообщает что третьей цифры нет
+
+Console.WriteLine("Введите любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+
+if (number > 99 && number < 1000)
+{
+    int x = number % 10;
+    Console.WriteLine($"Третья цифра - {x}");
+}
+else if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+
+    int result = number % 10;
+    Console.WriteLine($"Третья цифра - {result}");
+}
+
