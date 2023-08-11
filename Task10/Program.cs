b6c1a3d388fb0c1854eff850a6a2e9cddd0e1d2a
Denis <moiseev-denis@mail.ru>
2023-08-11 11:45:24 +0300

    Решил задачу 10

---
+++ Task10/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программ принимает на вход трехзначное число 
+// и сообщает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 1000) 
+    Console.WriteLine($"Вторая цифра введенного числа - {GetSecondDigit(number)}");
+else    
+    Console.WriteLine("Некорректный ввод");
+
+int randNum = new Random().Next(100, 1000);
+Console.WriteLine($"\n Случайное трехзначное число - {randNum}");
+Console.WriteLine($"Вторая цифра случайного числа - {GetSecondDigit(randNum)}");
+
+
+int GetSecondDigit(int num)
+{
+    int result = (num / 10) % 10;
+    return result;
+}
