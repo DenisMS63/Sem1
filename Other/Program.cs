d79e761b6e7af088264a242deedc0898378ca3ec
Denis <moiseev-denis@mail.ru>
2023-08-25 12:11:11 +0300

    Дописал, но не работает

--- Other/Program.cs
+++ Other/Program.cs
@@ -6,25 +6,37 @@ Console.WriteLine("Массив из случайных чисел");
 
 void RandArr(int n)
 {
-    int[] arr = new int [n];
+    int[] arr = new int[n];
     Random rnd = new Random();
 
     for (int i = 0; i < n; i++)
     {
         arr[i] = rnd.Next(1, 10);
         Console.Write($"{arr[i]} ");
-    }    
-}
-
-void SortArr (int [] ar)
-{
-    int min = ar [0];
-    int temp = ar [1];
-
-    for ( int i = 0; i < ar.Length; i ++)
-    {
-        for ( int j = 0; )
     }
 }
 
+void SortArr(int[] ar)
+{
+
+
+    for (int i = 0; i < ar.Length - 1; i++)
+    {
+        int minPos = i;
+
+        for (int j = i + 1; i < ar.Length; i++)
+        {
+            if (ar[j] < ar[minPos]) minPos = j;
+        }
+
+        int temp = ar[i];
+        ar[i] = ar[minPos];
+        ar[minPos] = temp;
+    }
+
+}
+
 RandArr(10);
+int [] array = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(SortArr(array));
