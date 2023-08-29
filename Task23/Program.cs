fc9cc26e132bd7aa14672ba549f00856cbe39bca
Denis <moiseev-denis@mail.ru>
2023-08-29 21:49:11 +0300

    Решил задачу 23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -4,14 +4,14 @@
 Console.WriteLine($"Введите число N");
 int inputN = Convert.ToInt32(Console.ReadLine());
 
-void TableOfSquares (int N, int x)
+void TableOfCubes (int N, int x)
 {
     Console.WriteLine($"Таблица квадратов чисел от 1 до N:");
     for (int i = 1; i <= N; i++)
     {
-        double square = Math.Pow(i, x);
-        Console.WriteLine($"{i, 3} -> {square, 5}");
+        double cube = Math.Pow(i, x);
+        Console.WriteLine($"{i, 3} -> {cube, 5}");
     }
 }
 
-TableOfSquares (inputN, 3);
+TableOfCubes (inputN, 3);
