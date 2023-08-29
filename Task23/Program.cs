55593db84aa10d69f66222ab5166de4775ee4d75
Denis <moiseev-denis@mail.ru>
2023-08-29 21:41:36 +0300

    Решил задачу 23

---
+++ Task23/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа принимает на вход число N и отображает таблицу 
+// кубов чисел от 1 до N
+
+Console.WriteLine($"Введите число N");
+int inputN = Convert.ToInt32(Console.ReadLine());
+
+void TableOfSquares (int N, int x)
+{
+    Console.WriteLine($"Таблица квадратов чисел от 1 до N:");
+    for (int i = 1; i <= N; i++)
+    {
+        double square = Math.Pow(i, x);
+        Console.WriteLine($"{i, 3} -> {square, 5}");
+    }
+}
+
+TableOfSquares (inputN, 3);
