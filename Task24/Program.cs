5a941a63b639b637a18c406c725a162a6de62c2a
Denis <moiseev-denis@mail.ru>
2023-08-30 01:29:49 +0300

    Решил задачу 24

---
+++ Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Программа принимает на вход число А и отображает сумму чисел
+// от 1 до А
+
+int SumOfNums(int x)
+{
+    int sum = 0;
+    for (int i = 1; i <= x; i++)
+    {
+        checked
+        {
+            sum += i;
+        }
+
+    }
+    return sum;
+}
+
+Console.WriteLine($"Введите число А");
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input == 0) Console.WriteLine($"Число А не должно быть равно нулю");
+else if (input < 0) Console.WriteLine($"Число А не может быть отрицательным");
+else
+{
+    Console.WriteLine($"Сумма чисел от 1 до А");
+    Console.WriteLine($"{input} -> {SumOfNums(input)}");
+}
+
