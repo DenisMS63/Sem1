f40f4e950b4403b1ab9b8ba8e7baca4f67201e63
Denis <moiseev-denis@mail.ru>
2023-08-30 12:53:51 +0300

    Решил задачу 26

---
+++ Task26/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает на вход число и сообщается количество цифр в числе
+
+int AmountOfNumbers(int n)
+{
+    if (n == 0) return 1;
+
+    int count = 0;
+    while (n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;
+
+}
+
+Console.WriteLine($"Введите длинное число");
+int input = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{input} -> {AmountOfNumbers(input)}");
