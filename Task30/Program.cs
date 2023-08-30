10e85ddafbc13e852fcccc4349c1cf61ec22b3d9
Denis <moiseev-denis@mail.ru>
2023-08-31 01:02:20 +0300

    Решил задачу 30

---
+++ Task30/Program.cs
@@ -0,0 +1,26 @@
+﻿// Программа отображает массив из 8 элементов, заполненный 
+//нулями и единицами в случайном порядке
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($" |");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+Console.WriteLine($"Массив из случайных нулей и единиц");
+PrintArray(array);
