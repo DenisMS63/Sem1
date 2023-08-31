162aba314580d2456abf9b31da3a3dde546b730f
Denis <moiseev-denis@mail.ru>
2023-08-31 12:14:42 +0300

    Решил задачу 25

---
+++ Task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа принимает два числа (А и В) и возводит
+// число А в натуральную степень В
+
+Console.Write($"Введите целое число А: ");
+int inputA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите натуральную степень B: ");
+int inputB = Convert.ToInt32(Console.ReadLine());
+
+int NumbertoPower(int A, int B)
+{
+    int result = 1;    
+    if (B == 0) return 1;
+    if (B == 1) return A;    
+    else
+    {
+        for (int i = 0; i < B; i++)
+        {
+            result *= A;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine($"Число {inputA} в степени {inputB} -> {NumbertoPower(inputA, inputB)}");
