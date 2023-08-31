b0154d942d1e46036b78b493a8f281dee66c53c1
Denis <moiseev-denis@mail.ru>
2023-08-31 13:12:30 +0300

    Решил задачу 27

---
+++ Task27/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа принимает на вход число и отображает сумму 
+// цифр из которых состоит введеное число
+
+int SumOfDigits(int n)
+{
+    int sum = 0;
+
+    if (n < 0)
+    {
+        n *= -1;
+        while (n != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
+    }
+    else
+    {
+        while (n != 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+    }
+    return sum;
+}
+
+Console.Write($"Введите длинное число: ");
+int input = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Сумма всех цифр числа {input} = {SumOfDigits(input)}");
