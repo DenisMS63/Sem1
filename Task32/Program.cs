5c25d736fa0066be3ec3ecf6f441f665e32a66a4
Denis <moiseev-denis@mail.ru>
2023-09-03 14:50:53 +0300

    Решил задачу 32

---
+++ Task32/Program.cs
@@ -0,0 +1,41 @@
+﻿// Программа создает массив и заменяет положительные элементы 
+// на отрицательные и на оборот
+
+int [] CreateRandomArray (int size, int min, int max)
+{
+    int [] arr = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int [] ChangeTheSignOfNumber (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+    return arr;   
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($" |");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($" {arr[i]} |");
+    }
+    Console.WriteLine();
+}
+
+int [] ar1 = CreateRandomArray (6, -5, 5);
+
+Console.WriteLine($"Массив из случайных элементов");
+PrintArray(ar1);
+
+
+Console.WriteLine($"Массив с обратными знаками элементов");
+PrintArray(ChangeTheSignOfNumber (ar1));
