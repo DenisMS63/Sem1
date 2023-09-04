32edff5c663bd1a96e9210de149612e87094004b
Denis <moiseev-denis@mail.ru>
2023-09-04 23:33:16 +0300

    Решил задачу 42

---
+++ Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿// Программа преобразовывает десятичное число в двоичное
+
+string DecimalNumToBinary (int num)
+{
+    string binary = string.Empty;
+
+    while (num > 0)
+    {
+        binary = num % 2 + binary;
+        num /= 2; 
+    } 
+    return binary;
+}
+
+int DecimalNumToBinary_2 (int num)
+{
+    int binary = 0;
+    int mult = 1;
+    while (num != 0)
+    {
+        binary += mult * (num % 2);
+        num /= 2;
+        mult *= 10; 
+    }
+    return binary;
+}
+
+Console.Write($"Введите десятичное число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine ($"Число {x} в двоичном представлении -> [ {DecimalNumToBinary (x)} ]");
+Console.WriteLine ($"Число {x} в двоичном представлении -> [ {DecimalNumToBinary_2 (x)} ]");
