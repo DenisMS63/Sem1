948f063184b92efd84c78676a52287068e7ac0f4
Denis <moiseev-denis@mail.ru>
2023-09-05 23:38:58 +0300

    Решил задачу 44

---
+++ Task44/Program.cs
@@ -0,0 +1,45 @@
+﻿// Не используя рекурсию, вывести первые числа Фибоначчи по числу N от пользователя
+
+int[] FibonacciNumbers(int n)
+{
+    int[] result = new int[n];
+    result[0] = 0;
+    result[1] = 1;
+
+    if (n == 2) return result;
+    for (int i = 2; i < n; i++)
+    {
+        checked
+        {
+            result[i] = result[i - 1] + result[i - 2];
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write($" [ ");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == arr.Length - 1) Console.Write($" {arr[i]}. ", 5);
+        else Console.Write($" {arr[i]}. | ", 5);
+    }
+    Console.WriteLine($" ]");
+}
+
+
+Console.Write($"Введите требуемое количество чисел Фибоначчи: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    int[] fibonacciNumbers = { 0 };
+    PrintArray(fibonacciNumbers);
+}
+else
+{
+    int[] fibonacciNumbers = FibonacciNumbers(num);
+    PrintArray(fibonacciNumbers);
+}
