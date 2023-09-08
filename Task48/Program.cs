c6eb1d061fb25cb8819f0784a6f23884613fcc16
Denis <moiseev-denis@mail.ru>
2023-09-08 14:18:35 +0300

    Решил задачу 48

---
+++ Task48/Program.cs
@@ -0,0 +1,36 @@
+﻿// Программа создает двумерный массив, каждый элемент массива 
+// находится по офрмуле [Amn = m + n] и затем отображает массив 
+
+
+int[,] CreateMatrixBySumIndex(int rows, int columns)
+{
+    int[,] arr = new int[rows, columns];
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = i + j;
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]}.", 5);           
+        }
+        Console.WriteLine($" ]\n"); 
+    }    
+}
+
+int [,] createMatrixBySumIndex = CreateMatrixBySumIndex(3, 4);
+
+Console.WriteLine($"\nДвумерный массив (сумма индексов)\n");
+PrintMatrix(createMatrixBySumIndex);
