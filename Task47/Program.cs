fcbdabbab49065cde2643ebd2445dd9d6a8f0100
Denis <moiseev-denis@mail.ru>
2023-09-08 15:39:26 +0300

    Решил задачу 47

---
+++ Task47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Программа создает двумерный случайный массив заполненный
+// вещественными числами
+
+double[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    double[,] arr = new double[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.NextDouble() * (max - min) + min;
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(double[,] arr)
+{
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]:F1}. ");
+        }
+        Console.WriteLine($" ]\n");
+    }
+}
+ 
+double[,] createRandomMatrix = CreateRandomMatrix(3, 4, -1, 5);
+
+Console.WriteLine($"Двумерный массив из вещественных чисел (рандомный)\n");
+PrintMatrix(createRandomMatrix);
