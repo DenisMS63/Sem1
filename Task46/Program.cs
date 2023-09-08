bef92c58bfc3cba0af5c160c95ed62e4ae154e28
Denis <moiseev-denis@mail.ru>
2023-09-08 13:58:28 +0300

    Решил задачу 46

---
+++ Task46/Program.cs
@@ -0,0 +1,35 @@
+﻿// Программа создает двумерный массив заполненный случайными числами
+// и затем отображает его в консоль
+
+int[,] CreateRandomMatrix(int rows, int columns, int min, int max)
+{
+    int[,] arr = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintMatrix(int[,] arr)
+{
+    
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.Write($"[ ");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($" {arr[i, j]}.", 5);           
+        }
+        Console.WriteLine($" ]\n"); 
+    }    
+}
+
+int [,] createRandomMatrix = CreateRandomMatrix(3, 4, 10, 99);
+
+Console.WriteLine($"Двумерный массив (рандомный)\n", 6);
+PrintMatrix(createRandomMatrix);
