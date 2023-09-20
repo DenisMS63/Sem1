f148dd0d7c9fdbf743980f1df80a857b1ccfb862
Denis <moiseev-denis@mail.ru>
2023-09-20 11:46:02 +0300

    Сделал сортировку

--- Other/Program.cs
+++ Other/Program.cs
@@ -1,38 +1,49 @@
 ﻿// Программа генерирует массив из случаный чисел 
 //и записывает значения в порядке возрастания  
 
-int [] RandArr(int n)
+int[] CreateRandomArray(int size, int min, int max)
 {
-    int[] arr = new int[n];
+    int[] arr = new int[size];
     Random rnd = new Random();
-
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        arr[i] = rnd.Next(1, 10);
-        Console.Write($"{arr[i]} ");
+        arr[i] = rnd.Next(min, max + 1);
     }
+    return arr;
 }
 
-void SortArr(int [] ar)
+void PrintArray(int[] arr)
 {
-
-
-    for (int i = 0; i < ar.Length - 1; i++)
+    Console.Write($" [ ");
+    for (int i = 0; i < arr.Length; i++)
     {
-        int minPos = i;
-
-        for (int j = i + 1; i < ar.Length; i++)
-        {
-            if (ar[j] < ar[minPos]) minPos = j;
-        }
-
-        int temp = ar[i];
-        ar[i] = ar[minPos];
-        ar[minPos] = temp;
+        if (i == arr.Length - 1) Console.Write($" {arr[i]}. ", 5);
+        else Console.Write($" {arr[i]}. ", 5);
     }
+    Console.WriteLine($"]");
+}
 
+void SortArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        for (int j = i + 1; j < arr.Length; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
 }
 
 Console.WriteLine("Массив из случайных чисел");
 
-SortArr(RandArr(10));
+int[] createRandomArray = CreateRandomArray(5, 1, 10);
+PrintArray(createRandomArray);
+
+SortArr(createRandomArray);
+// Array.Sort(createRandomArray);
+PrintArray(createRandomArray);
