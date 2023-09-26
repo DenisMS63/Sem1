7fb587ab9549b527db1c1c91610d6503817943cb
Denis <moiseev-denis@mail.ru>
2023-09-26 13:28:36 +0300

    Решил задачу 65

---
+++ Task65/Program.cs
@@ -0,0 +1,30 @@
+﻿// Программа выводить все числа от M до N
+
+Console.Write($"Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+NumberFromMtoN (numberM, numberN);
+
+void NumberFromMtoN (int m, int n)
+{
+    if (m < n)
+    {
+        
+        Console.Write($"{m} ");
+        NumberFromMtoN(m + 1, n);
+        
+    }
+    else if (n < m)
+    {
+        
+        Console.Write($"{m} ");
+        NumberFromMtoN(m - 1, n);
+    }
+    else
+    {
+        Console.Write($"{m}");
+    }
+}
