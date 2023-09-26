044f0665fb4f8459af2c8088b429b170ed4e5c9e
Denis <moiseev-denis@mail.ru>
2023-09-26 12:36:41 +0300

    Решил задачу 63

---
+++ Task63/Program.cs
@@ -0,0 +1,14 @@
+﻿// РЕКУРСИЯ
+// Программа выводит все значения от 1 до N
+
+Console.Write($"Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NumbersFrom1toN (number);
+
+
+void NumbersFrom1toN (int n)
+{
+    if (n == 0) return;
+    NumbersFrom1toN(n - 1); // числа в обратном порядке
+    Console.Write($"{n} ");
+}
