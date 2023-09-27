7b8ad949e7c04ce40755bc94b506e9c858e4d6d8
Denis <moiseev-denis@mail.ru>
2023-09-27 13:00:50 +0300

    Решил задачу 64

---
+++ Task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// РЕКУРСИЯ Программа принимает на вход число N и выводит все натуральные числа 
+// в промежутке от N до 1
+
+Console.Write($"Введите натуральное число N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+NumbersFrom1toN (number);
+
+
+void NumbersFrom1toN (int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n} ");
+    NumbersFrom1toN(n - 1); // числа в обратном порядке складываются в стек
+    
+}
