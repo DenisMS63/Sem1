933e464f886ba42ed3bf01c772fb69e01ce73bf1
Denis <moiseev-denis@mail.ru>
2023-09-27 14:15:06 +0300

    Решил задачу 68

---
+++ Task68/Program.cs
@@ -0,0 +1,25 @@
+﻿// РЕКУРСИЯ Программа для вычисления функции Акермана, даны два 
+// неотрицательных числа M и N
+
+int AckermanFunction (int n, int m)
+{
+    if (n == 0) 
+    {
+        return m + 1;
+    }
+    else if (n != 0 && m == 0) 
+    {
+        
+        return AckermanFunction (n - 1, 1);
+    }
+    else return AckermanFunction (n - 1, AckermanFunction (n, m - 1));
+}
+
+Console.Write($"Введите целое неотрицательное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Введите целое неотрицательное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int ackermanFunction = AckermanFunction (numberN, numberM);
+Console.Write($"Результат вычисления функции Акермана -> {ackermanFunction}");
