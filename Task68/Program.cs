6c61fe09178703bd66ad0c31802a0a8800830ba5
Denis <moiseev-denis@mail.ru>
2023-10-08 13:46:57 +0300

    Решил задачу 68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -3,15 +3,8 @@
 
 int AckermanFunction (int n, int m)
 {
-    if (n == 0) 
-    {
-        return m + 1;
-    }
-    else if (n != 0 && m == 0) 
-    {
-        
-        return AckermanFunction (n - 1, 1);
-    }
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return AckermanFunction (n - 1, 1); 
     else return AckermanFunction (n - 1, AckermanFunction (n, m - 1));
 }
 
